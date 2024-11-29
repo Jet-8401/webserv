a1d6da1f57e13bf18d260d3a4b0155de645c00a3
Maxime De Prins <mde-prin@c1r2s3.42luxembourg.lu>
2024-11-29 16:53:12 +0100

    root and alias problems

--- srcs/HttpResponse.cpp
+++ srcs/HttpResponse.cpp
@@ -75,15 +75,17 @@ int	HttpResponse::_resolveLocation(std::string& path, struct stat& file_stats, c
 		path = joinPath(this->_location->getRoot(), request_location);
 
 	// know test for multiples index if there is
-	std::string	full_path;
-	for (std::vector<std::string>::const_iterator it = indexes.begin(); it != indexes.end(); it++) {
-		full_path = joinPath(path, *it);
-		if (::stat(full_path.c_str(), &file_stats) == -1) {
-			::memset(&file_stats, 0, sizeof(file_stats));
-			continue;
-		} else {
-			path = full_path;
-			return (0);
+	if (this->_action & SENDING_MEDIA) {
+		std::string	full_path;
+		for (std::vector<std::string>::const_iterator it = indexes.begin(); it != indexes.end(); it++) {
+			full_path = joinPath(path, *it);
+			if (::stat(full_path.c_str(), &file_stats) == -1) {
+				::memset(&file_stats, 0, sizeof(file_stats));
+				continue;
+			} else {
+				path = full_path;
+				return (0);
+			}
 		}
 	}
 
@@ -99,7 +101,7 @@ int	HttpResponse::_resolveRequest(const HttpRequest& request)
 {
 	if (this->_resolveLocation(this->_complete_path, this->_media_stat, request.getLocation()) == -1)
 		return (-1);
-
+	std::cout << this->_complete_path << std::endl;
 	// Check for autoindex.
 	if (S_ISDIR(this->_media_stat.st_mode)) {
 		this->_dir = ::opendir(this->_complete_path.c_str());
