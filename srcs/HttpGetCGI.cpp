c11e42f53e9aebab4105207325e1662b776be5f2
Maxime De Prins <mde-prin@c1r1s6.42luxembourg.lu>
2024-12-10 18:20:09 +0100

    Simplified the code of HttpGetCGI class

--- srcs/HttpGetCGI.cpp
+++ srcs/HttpGetCGI.cpp
@@ -11,13 +11,18 @@
 
 HttpGetCGI::HttpGetCGI(const HttpParser& parser):
 	HttpParser(parser),
-	_cgi_pid(-1)
+	_cgi_pid(-1),
+	_headers("HTTP/1.1 200 OK\r\n"
+			"Content-Type: text/html\r\n"
+			"Connection: keep-alive\r\n"
+			"\r\n"),
+	_headers_pos(0)
 {
 	if (pipe(this->_pipe_out) == -1) {
 		this->_request.error(500);
 		return;
 	}
-	this->_request.setEvents(EPOLLOUT);  // Set to EPOLLOUT to enable write
+	this->_request.setEvents(EPOLLOUT);
 	this->executeCGI();
 }
 
@@ -71,42 +76,18 @@ bool	HttpGetCGI::parse(const uint8_t* packet, const size_t packet_size)
 
 ssize_t	HttpGetCGI::write(const uint8_t* io_buffer, const size_t buff_length)
 {
-	static bool headers_sent = false;
-	static std::string headers =	"HTTP/1.1 200 OK\r\n"
-									"Content-Type: text/html\r\n"
-									"Connection: keep-alive\r\n"
-									"\r\n";
-	static size_t headers_pos = 0;
-
-	DEBUG("Write called with buffer length: " << buff_length);
-
-	if (!headers_sent) {
-		size_t remaining = headers.length() - headers_pos;
-		size_t to_send = std::min(remaining, buff_length);
-
-		memcpy(const_cast<uint8_t*>(io_buffer), headers.c_str() + headers_pos, to_send);
-		headers_pos += to_send;
-
-		if (headers_pos >= headers.length()) {
-			headers_sent = true;
-		}
-
-		DEBUG("Sending headers chunk: " << to_send << " bytes");
+	if (_headers_pos < _headers.length()) {
+		size_t to_send = std::min(_headers.length() - _headers_pos, buff_length);
+		memcpy(const_cast<uint8_t*>(io_buffer), _headers.c_str() + _headers_pos, to_send);
+		_headers_pos += to_send;
 		return to_send;
 	}
 
 	ssize_t bytes_read = read(this->_pipe_out[0],
 		const_cast<uint8_t*>(io_buffer), buff_length);
 
-	DEBUG("Read from pipe: " << bytes_read << " bytes");
-
-	if (bytes_read <= 0) {
-		int status;
-		waitpid(this->_cgi_pid, &status, WNOHANG);
-		if (WIFEXITED(status)) {
-			this->_state = handler_state_t(DONE, true);
-		}
-	}
+	if (bytes_read <= 0 && WIFEXITED(waitpid(this->_cgi_pid, NULL, WNOHANG)))
+		this->_state = handler_state_t(DONE, true);
 
 	return bytes_read;
 }
